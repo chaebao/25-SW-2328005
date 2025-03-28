0b27173eba2b7acf039c528636945ca5f0850f36
chaebao <oottychae@naver.com>
2025-03-28 10:41:53 +0900

    소스코드 업로드

---
+++ main.py
@@ -0,0 +1,51 @@
+#
+# num=input('정수 혹은 소수를 입력하세요: ')
+#
+# float_num=float(num)
+# print(f'입력한 숫자의 소수형: {float_num}')
+#
+# int_num=int(float(num))
+#
+# print(f'입력한 숫자의 정수형: {int_num}')
+
+# string=input('영어로 아무거나 입력하세요 (5자 이상): ')
+# print(f'str.upper(): {string.upper()}')
+# print(f'str.lower(): {string.lower()}')
+# print(f'str[2:4]: {string[2:4]}')
+#
+# l= list()
+# l.append(4)
+# print(l)
+# l.append('string')
+# print(l)
+# l.append(10.44)
+# print(l)
+# l.remove('string')
+# print(l)
+#
+# y={1,2,3,4,5,6,7,8,9}
+# print(y)
+#
+# y=set()
+# for n in range(0,20):
+#     y.add(n)
+# print(y)
+#
+# d= dict()
+#
+# d['key']=' Value'
+# print(d)
+# d[4]='string'
+# print(d)
+# d[20.2]=99.4
+# print(d)
+# d.pop(4)
+# print(d)
+#
+#  멜론 in keys()
+#  x.values
+#  x.items
+
+x=int(input())
+y=int(input())
+print(f'{x}+{y}={x+y}')
